d37633855f7bf7122fcc2d34a5be91a1d0c87b7d
Tobias Bieniek <tobias@bieniek.cloud>
2021-10-27 21:33:03 +0200

    ESLint: Update `node` overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,16 +25,19 @@ module.exports = {
     // node files
     {
       files: [
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js',
-        '.eslintrc.js',
+        './.ember-cli.js',
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './lib/*/index.js',
+        './server/**/*.js',
       ],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015,
       },
       env: {
         browser: false,

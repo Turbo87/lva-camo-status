3a58d349dbcfc3a005c696e962ab482303ca5875
Tobias Bieniek <tobias@bieniek.cloud>
2021-10-27 20:16:05 +0200

    tests: Extract `RESPONSE` constants

--- tests/integration/components/aircraft-status-test.js
+++ tests/integration/components/aircraft-status-test.js
@@ -5,6 +5,46 @@ import hbs from 'htmlbars-inline-precompile';
 import Pretender from 'pretender';
 import RSVP from 'rsvp';
 
+const AIRWORTHY_RESPONSE = {
+  'data': {
+    'id': 'WTsHJRdZ',
+    'type': 'status',
+    'attributes': {
+      'timestamp': '2018-05-30T08:13:21+00:00',
+      'camo': 'airworthy',
+      'ato': 'unknown',
+      'easa-type': 'ASK 21',
+      'easa-variant': 'ASK 21'
+    },
+    'relationships': {'aircraft': {'data': {'id': 'WTsHJRdZ', 'type': 'aircraft'}}},
+    'included': [{
+      'id': 'WTsHJRdZ',
+      'type': 'aircraft',
+      'attributes': {'easa-type': 'ASK 21', 'easa-variant': 'ASK 21'}
+    }]
+  }
+};
+
+let GROUNDED_RESPONSE = {
+  'data': {
+    'id': 'WTsHJRdZ',
+    'type': 'status',
+    'attributes': {
+      'timestamp': '2018-05-30T08:13:21+00:00',
+      'camo': 'grounded',
+      'ato': 'unknown',
+      'easa-type': 'ASK 21',
+      'easa-variant': 'ASK 21'
+    },
+    'relationships': {'aircraft': {'data': {'id': 'WTsHJRdZ', 'type': 'aircraft'}}},
+    'included': [{
+      'id': 'WTsHJRdZ',
+      'type': 'aircraft',
+      'attributes': {'easa-type': 'ASK 21', 'easa-variant': 'ASK 21'}
+    }]
+  }
+};
+
 module('Integration | Component | aircraft-status', function(hooks) {
   setupRenderingTest(hooks);
 
@@ -18,27 +58,7 @@ module('Integration | Component | aircraft-status', function(hooks) {
 
   test('it renders airworthy correctly', async function(assert) {
     this.server.get('https://api.camo-europe.aero/statuses/WTsHJRdZ', function() {
-      let response = {
-        'data': {
-          'id': 'WTsHJRdZ',
-          'type': 'status',
-          'attributes': {
-            'timestamp': '2018-05-30T08:13:21+00:00',
-            'camo': 'airworthy',
-            'ato': 'unknown',
-            'easa-type': 'ASK 21',
-            'easa-variant': 'ASK 21'
-          },
-          'relationships': {'aircraft': {'data': {'id': 'WTsHJRdZ', 'type': 'aircraft'}}},
-          'included': [{
-            'id': 'WTsHJRdZ',
-            'type': 'aircraft',
-            'attributes': {'easa-type': 'ASK 21', 'easa-variant': 'ASK 21'}
-          }]
-        }
-      };
-
-      return [200, {"Content-Type": "application/json"}, JSON.stringify(response)];
+      return [200, {"Content-Type": "application/json"}, JSON.stringify(AIRWORTHY_RESPONSE)];
     });
 
     await render(hbs`<AircraftStatus @id="WTsHJRdZ" @callsign="D-8784"/>`);
@@ -50,27 +70,7 @@ module('Integration | Component | aircraft-status', function(hooks) {
 
   test('it renders grounded correctly', async function(assert) {
     this.server.get('https://api.camo-europe.aero/statuses/WTsHJRdZ', function() {
-      let response = {
-        'data': {
-          'id': 'WTsHJRdZ',
-          'type': 'status',
-          'attributes': {
-            'timestamp': '2018-05-30T08:13:21+00:00',
-            'camo': 'grounded',
-            'ato': 'unknown',
-            'easa-type': 'ASK 21',
-            'easa-variant': 'ASK 21'
-          },
-          'relationships': {'aircraft': {'data': {'id': 'WTsHJRdZ', 'type': 'aircraft'}}},
-          'included': [{
-            'id': 'WTsHJRdZ',
-            'type': 'aircraft',
-            'attributes': {'easa-type': 'ASK 21', 'easa-variant': 'ASK 21'}
-          }]
-        }
-      };
-
-      return [200, {"Content-Type": "application/json"}, JSON.stringify(response)];
+      return [200, {"Content-Type": "application/json"}, JSON.stringify(GROUNDED_RESPONSE)];
     });
 
     await render(hbs`<AircraftStatus @id="WTsHJRdZ" @callsign="D-8784"/>`);
@@ -82,27 +82,7 @@ module('Integration | Component | aircraft-status', function(hooks) {
 
   test('can override aircraft type', async function(assert) {
     this.server.get('https://api.camo-europe.aero/statuses/WTsHJRdZ', function() {
-      let response = {
-        'data': {
-          'id': 'WTsHJRdZ',
-          'type': 'status',
-          'attributes': {
-            'timestamp': '2018-05-30T08:13:21+00:00',
-            'camo': 'airworthy',
-            'ato': 'unknown',
-            'easa-type': 'ASK 21',
-            'easa-variant': 'ASK 21'
-          },
-          'relationships': {'aircraft': {'data': {'id': 'WTsHJRdZ', 'type': 'aircraft'}}},
-          'included': [{
-            'id': 'WTsHJRdZ',
-            'type': 'aircraft',
-            'attributes': {'easa-type': 'ASK 21', 'easa-variant': 'ASK 21'}
-          }]
-        }
-      };
-
-      return [200, {"Content-Type": "application/json"}, JSON.stringify(response)];
+      return [200, {"Content-Type": "application/json"}, JSON.stringify(AIRWORTHY_RESPONSE)];
     });
 
     await render(hbs`<AircraftStatus @id="WTsHJRdZ" @callsign="D-8784" @type="AlphaJet"/>`);
@@ -116,27 +96,7 @@ module('Integration | Component | aircraft-status', function(hooks) {
     let deferred = RSVP.defer();
 
     this.server.get('https://api.camo-europe.aero/statuses/WTsHJRdZ', function() {
-      let response = {
-        'data': {
-          'id': 'WTsHJRdZ',
-          'type': 'status',
-          'attributes': {
-            'timestamp': '2018-05-30T08:13:21+00:00',
-            'camo': 'airworthy',
-            'ato': 'unknown',
-            'easa-type': 'ASK 21',
-            'easa-variant': 'ASK 21'
-          },
-          'relationships': {'aircraft': {'data': {'id': 'WTsHJRdZ', 'type': 'aircraft'}}},
-          'included': [{
-            'id': 'WTsHJRdZ',
-            'type': 'aircraft',
-            'attributes': {'easa-type': 'ASK 21', 'easa-variant': 'ASK 21'}
-          }]
-        }
-      };
-
-      return deferred.promise.then(() => [200, {"Content-Type": "application/json"}, JSON.stringify(response)]);
+      return deferred.promise.then(() => [200, {"Content-Type": "application/json"}, JSON.stringify(AIRWORTHY_RESPONSE)]);
     });
 
     render(hbs`<AircraftStatus @id="WTsHJRdZ" @callsign="D-8784"/>`);
